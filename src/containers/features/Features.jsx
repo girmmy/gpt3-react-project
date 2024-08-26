dd1f57b9ebfda070adba3fc168982a740a613f8b
gimmy <gimmys943@gmail.com>
2024-08-25 21:08:32 -0400

    initial commit

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './features.css'
+
+
+const Features = ({ title, text }) => {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-contanier__feature">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container">
+        {text}
+      </div>
+    </div>
+  )
+}
+
+export default Features
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './whatGPT3.css'
+import { Feature } from '../../components'
+
+const whatGPT3 = () => {
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  )
+}
+
+export default whatGPT3
